e7c83eaa60c0f83419e63b363e4492bcf91061bf
G. Georgopoulos <georgopoulos.georgios@gmail.com>
2014-10-06 12:54:28 +0300

    Added support for process.env.FREDDO_DTALK_MAC.

--- freddo-dtalk-server/freddo-dtalk-server.js
+++ freddo-dtalk-server/freddo-dtalk-server.js
@@ -7,8 +7,10 @@ var DTalkService = require('./dtalk/dtalk-service.js')
 
 var port = process.env.FREDDO_DTALK_PORT || 8840;
 var name = process.env.FREDDO_DTALK_NAME || os.hostname();
+var mac  = process.env.FREDDO_DTALK_MAC  || uuid.v1();
+
 DTalkService.start({ 
-	name: name + '@' + uuid.v1(), 
+	name: name + '@' + mac, 
 	port: port, 
 	dtype: 'RaspberryPi/1;',
 	www: __dirname + '/www/'
